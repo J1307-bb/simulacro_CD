2624332747035ef8fa2bc736c165e35b9fb49c33
J1307-bb <edgarjair2001@hotmail.com>
2024-11-07 18:44:44 -0500

    Nueva funcion: Despedida

--- app.py
+++ app.py
@@ -2,5 +2,9 @@
 def saludo():
     return "Hola, Mundo"
 
+def despedida():
+    return "Adiós, Mundo"
+
 if __name__ == "__main__":
     print(saludo())
+    print(despedida())
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,13 @@
 
 import unittest
 from app import saludo
+from app import despedida
 
 class TestApp(unittest.TestCase):
     def test_saludo(self):
         self.assertEqual(saludo(), "Hola, Mundo")
+    def test_despedida(self):
+        self.assertEqual(despedida(), "Adiós, Mundo")
 
 if __name__ == "__main__":
     unittest.main()
